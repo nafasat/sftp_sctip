5fa3d1f713a9e58b88e80ee7dd51dbc55690482b
nafasat <41534389+nafasat@users.noreply.github.com>
2022-01-27 22:15:04 +0530

    Update sftp.sh

--- sftp.sh
+++ sftp.sh
@@ -5,6 +5,7 @@ Sftp_Key=$3
 Sftp_IP='34.131.87.94'
 
 echo "get $File_Name" > command.txt
+echo "put command.txt" > command.txt
 
 sftp -oStrictHostKeyChecking=no -i $Sftp_Key -b command.txt ${Sftp_User}@${Sftp_IP}
 
