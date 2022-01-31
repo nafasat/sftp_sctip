3339b99f42972552501d56026a64b6395a3967b1
nafasat <41534389+nafasat@users.noreply.github.com>
2022-01-31 11:39:24 +0530

    remove command file

--- sftp.sh
+++ sftp.sh
@@ -4,10 +4,10 @@ Sftp_User=$2
 Sftp_Key=$3
 Sftp_IP='34.131.87.94'
 
-echo "get $File_Name" > command.txt
-echo "put command.txt" > command.txt
+# echo "get $File_Name" > command.txt
+# echo "put command.txt" > command.txt
 
-sftp -oStrictHostKeyChecking=no -i $Sftp_Key -b command.txt ${Sftp_User}@${Sftp_IP}
+echo "get $File_Name" | sftp -oStrictHostKeyChecking=no -i $Sftp_Key ${Sftp_User}@${Sftp_IP}
 
 if [ $? -ne 0 ]
 then
@@ -16,4 +16,3 @@ then
 else
   ls $File_Name
 fi
-
